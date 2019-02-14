06d85344c956dd209f4b0896f89d9f280c1ff76f
albin <albin.warnicke@live.se>
2019-02-14 21:33:35 +0100

    v.3.1 Graferna skapas nu i en egen klass Graph. Har även tagit bort scrollningen och zoomningen i y-led.

---
+++ app/src/main/java/com/example/RadarHealthMonitoring/Graph.java
@@ -0,0 +1,63 @@
+package com.example.RadarHealthMonitoring;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.Toast;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.Series;
+
+/**
+ * Skapar graferna
+ */
+class Graph {
+
+    private double dataNumber = 0;
+    private LineGraphSeries<DataPoint> series;
+    private Context context;
+
+    /**
+     * Konstruktor
+     * @param view
+     * @param context
+     */
+    Graph(View view, final Context context) {
+        //GraphView graph = (GraphView) findViewById(R.id.graphPulse);
+        GraphView graph = (GraphView) view;
+        series = new LineGraphSeries<>(new DataPoint[]{      //skapar en första mätserie
+                new DataPoint(0, 0)
+        });
+        series.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                //Context context = getApplicationContext();
+                Toast.makeText(context, "Value "+dataPoint, Toast.LENGTH_SHORT).show();
+            }
+        });
+        graph.addSeries(series);        //lägger till serien med mätvärden till grafen.
+        graph.getViewport().setScrollable(true);        //scrollable in horizontal (x-axis)
+        //graph.getViewport().setScrollableY(true);
+        graph.getViewport().setScalable(true);      //för zoomning
+        //graph.getViewport().setScalableY(true);
+        graph.getViewport().setXAxisBoundsManual(true);     //set Viewport window size
+        graph.getViewport().setYAxisBoundsManual(true);
+        //graph.getViewport().setMaxXAxisSize(1);
+        //graph.getViewport().setMinY(-1.5);      //begränsar y-axeln med värden
+        //graph.getViewport().setMaxY(1.5);
+        //graph.getGridLabelRenderer().setHorizontalAxisTitle("Time (s)");
+        //graph.getGridLabelRenderer().setVerticalAxisTitle("Heartrate (beats/min)");
+        //graph.setTitle("Heartrate");
+    }
+
+    /**
+     * Getter för Serierna
+     * @return series
+     */
+    LineGraphSeries<DataPoint> getSeries() {
+        return series;
+    }
+}
--- app/src/main/java/com/example/RadarHealthMonitoring/MainActivity.java
+++ app/src/main/java/com/example/RadarHealthMonitoring/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.RadarHealthMonitoring;
 
-import android.content.Context;
 import android.content.Intent;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -10,23 +9,18 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-
-//GraphView
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.DataPointInterface;
-import com.jjoe64.graphview.series.LineGraphSeries;
-import com.jjoe64.graphview.series.OnDataPointTapListener;
-import com.jjoe64.graphview.series.Series;
+
 
 /**
  * Huvudaktiviteten för appen
  */
 public class MainActivity extends AppCompatActivity {
+
     private double dataNumber = 0;
-    private LineGraphSeries<DataPoint> seriesPulse;
-    private LineGraphSeries<DataPoint> seriesBreathe;
+    private Graph graphPulse;
+    private Graph graphBreathe;
+
     /**
      * Skapar huvudfönstret
      */
@@ -35,57 +29,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         PreferenceManager.setDefaultValues(this, R.xml.settings, false); // så systemet inte sätter default
-
-        //Graphs
-        GraphView graphPulse = (GraphView) findViewById(R.id.graphPulse);
-        seriesPulse = new LineGraphSeries<>(new DataPoint[]{      //skapar en första mätserie
-                new DataPoint(0, 0)
-        });
-        seriesPulse.setOnDataPointTapListener(new OnDataPointTapListener() {
-            @Override
-            public void onTap(Series series, DataPointInterface dataPoint) {
-                Context context = getApplicationContext();
-                Toast.makeText(context, "Value "+dataPoint, Toast.LENGTH_SHORT).show();
-            }
-        });
-        graphPulse.addSeries(seriesPulse);        //lägger till serien med mätvärden till grafen.
-        graphPulse.getViewport().setScrollable(true);        //scrollable in horizontal (x-axis)
-        graphPulse.getViewport().setScrollableY(true);
-        graphPulse.getViewport().setScalable(true);      //för zoomning
-        graphPulse.getViewport().setScalableY(true);
-        graphPulse.getViewport().setXAxisBoundsManual(true);     //set Viewport window size
-        graphPulse.getViewport().setYAxisBoundsManual(true);
-        //graph.getViewport().setMaxXAxisSize(1);
-        //graph.getViewport().setMinY(-1.5);      //begränsar y-axeln med värden
-        //graph.getViewport().setMaxY(1.5);
-        graphPulse.getGridLabelRenderer().setHorizontalAxisTitle("Time (s)");
-        //graphPulse.getGridLabelRenderer().setVerticalAxisTitle("Heartrate (beats/min)");
-        //graphPulse.setTitle("Heartrate");
-
-        GraphView graphBreathe = (GraphView) findViewById(R.id.graphBreathe);
-        seriesBreathe = new LineGraphSeries<>(new DataPoint[]{      //skapar en första mätserie
-                new DataPoint(0, 0)
-        });
-        seriesBreathe.setOnDataPointTapListener(new OnDataPointTapListener() {
-            @Override
-            public void onTap(Series series, DataPointInterface dataPoint) {
-                Context context = getApplicationContext();
-                Toast.makeText(context, "Value "+dataPoint, Toast.LENGTH_SHORT).show();
-            }
-        });
-        graphBreathe.addSeries(seriesBreathe);        //lägger till serien med mätvärden till grafen.
-        graphBreathe.getViewport().setScrollable(true);        //scrollable in horizontal (x-axis)
-        graphBreathe.getViewport().setScrollableY(true);
-        graphBreathe.getViewport().setScalable(true);      //för zoomning
-        graphBreathe.getViewport().setScalableY(true);
-        graphBreathe.getViewport().setXAxisBoundsManual(true);     //set Viewport window size
-        graphBreathe.getViewport().setYAxisBoundsManual(true);
-        //graph.getViewport().setMaxXAxisSize(1);
-        //graph.getViewport().setMinY(-1.5);      //begränsar y-axeln med värden
-        //graph.getViewport().setMaxY(1.5);
-        graphBreathe.getGridLabelRenderer().setHorizontalAxisTitle("Time (s)");
-        //graphBreathe.getGridLabelRenderer().setVerticalAxisTitle("Breathing rate (beats/min)");
-        //graphBreathe.setTitle("Breathing rate");
+        /* Graphs */
+        graphPulse = new Graph(findViewById(R.id.graphPulse),getApplicationContext());
+        graphBreathe = new Graph(findViewById(R.id.graphBreathe),getApplicationContext());
     }
 
     /**
@@ -113,11 +59,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     public void addData(View view) {        //knapp för att lägga till ett värde till serien
-        double y = Math.sin(dataNumber);
-        DataPoint data = new DataPoint(dataNumber,y);
-        seriesPulse.appendData(data, true,100);
-        seriesBreathe.appendData(data, true,100);
+        double yPulse = Math.sin(dataNumber*2)/2+0.5;
+        double yBreathe = Math.sin(dataNumber)/2+0.5;
+        DataPoint dataPulse = new DataPoint(dataNumber,yPulse);
+        DataPoint dataBreathe = new DataPoint(dataNumber,yBreathe);
+        graphPulse.getSeries().appendData(dataPulse, true,100); // seriesPulse
+        graphBreathe.getSeries().appendData(dataBreathe, true,100); // seriesBreathe
         dataNumber += 0.5;
     }
-
 }
